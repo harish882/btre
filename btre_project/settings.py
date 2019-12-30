ea18e1eb5e5cd9b3df4f0cda7417984adc4404f0
harish882 <harishguntaka@gmail.com>
2019-12-30 13:54:47 +0530

    settings file and runtime file

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-     'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
